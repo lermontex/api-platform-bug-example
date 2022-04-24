32d108ed17db62b663da606a2d9d3bd88392bda2
lermont2301 <lermont2301@mail.ru>
2022-04-24 04:31:55 +0300

    Add Fixtures

--- src/DataFixtures/AppFixtures.php
+++ src/DataFixtures/AppFixtures.php
@@ -2,6 +2,9 @@
 
 namespace App\DataFixtures;
 
+use App\Entity\Domain;
+use App\Entity\ResetTokenRequest;
+use App\Entity\User;
 use Doctrine\Bundle\FixturesBundle\Fixture;
 use Doctrine\Persistence\ObjectManager;
 
@@ -9,8 +12,18 @@ class AppFixtures extends Fixture
 {
     public function load(ObjectManager $manager): void
     {
-        // $product = new Product();
-        // $manager->persist($product);
+        $user = new User();
+        $manager->persist($user);
+
+        $domain = new Domain();
+        $domain->setUser($user);
+        $domain->setApiToken('test');
+        $domain->setIsActive(true);
+        $manager->persist($domain);
+
+        $resetTokenRequest = new ResetTokenRequest();
+        $resetTokenRequest->setDomain($domain);
+        $manager->persist($resetTokenRequest);
 
         $manager->flush();
     }
